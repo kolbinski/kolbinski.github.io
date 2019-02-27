0d886ab5b154e16d8ffba2e8768c26fcf5baab2f
kolbinski <olbik80@gmail.com>
2019-02-27 15:32:07 +0100

    Update index.js

--- simple-button/index.js
+++ simple-button/index.js
@@ -1,7 +1,7 @@
 FooHTML
   .register('simple-button', {
 		events: {
-			click: () => {
+			click: function () {
 				this.style.backgroundColor = 'gold';
 				this.innerHTML = 'I was clicked!';
 			}
