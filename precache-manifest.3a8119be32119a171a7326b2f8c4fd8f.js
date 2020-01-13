eca10519263564878f5b8721aa95d959101558c2
Krzysztof Olbinski <olbinski@gmail.com>
2020-01-13 15:13:02 +0100

    travelio

---
+++ precache-manifest.3a8119be32119a171a7326b2f8c4fd8f.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d4964f52f8dee671ce41ad12bdcd28e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8961935c56d9fb161ccd",
+    "url": "/static/css/2.96479aa2.chunk.css"
+  },
+  {
+    "revision": "e9dc15cc501cf82584dd",
+    "url": "/static/css/main.a91e5db2.chunk.css"
+  },
+  {
+    "revision": "8961935c56d9fb161ccd",
+    "url": "/static/js/2.66de8c27.chunk.js"
+  },
+  {
+    "revision": "4fe4fb8c3b519c29c8e94ad47857d492",
+    "url": "/static/js/2.66de8c27.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e9dc15cc501cf82584dd",
+    "url": "/static/js/main.6755a7ad.chunk.js"
+  },
+  {
+    "revision": "e35bfabb023e3faa1f43",
+    "url": "/static/js/runtime-main.af97d5a7.js"
+  },
+  {
+    "revision": "daea361f4621e190095631d6d5df680a",
+    "url": "/static/media/Flighttix.de.daea361f.png"
+  },
+  {
+    "revision": "daea361f4621e190095631d6d5df680a",
+    "url": "/static/media/Flighttix.pl.daea361f.png"
+  },
+  {
+    "revision": "5701cc603fd9529d8e6e1e278e0b2e72",
+    "url": "/static/media/Tix.fr.5701cc60.png"
+  },
+  {
+    "revision": "57e9ba7449aea96c21a38097d38422df",
+    "url": "/static/media/about-us.57e9ba74.jpg"
+  },
+  {
+    "revision": "13795980dffadfadc54f963f05f2c8d6",
+    "url": "/static/media/bg1.13795980.jpg"
+  },
+  {
+    "revision": "81ad8f4c41a28d01330ded62c686372e",
+    "url": "/static/media/logo.81ad8f4c.png"
+  }
+]);
